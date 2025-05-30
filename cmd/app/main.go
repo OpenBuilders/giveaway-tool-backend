8c0ba5b2fe04ff5333837ca37aaf3fb2f1f71e24
Seinarukiro <seinarukiro@gmail.com>
2025-05-30 22:06:15 +0200

    feat: add production domain to CORS configuration

--- cmd/app/main.go
+++ cmd/app/main.go
@@ -115,7 +115,7 @@ func main() {
 	router := gin.New()
 	router.Use(
 		cors.New(cors.Config{
-			AllowOrigins:     []string{"http://localhost:3000"},
+			AllowOrigins:     []string{"http://localhost:3000", "https://giveaway.kopasxa.me"},
 			AllowMethods:     []string{"GET", "POST", "PUT", "DELETE", "OPTIONS"},
 			AllowHeaders:     []string{"Content-Type", "Authorization", "Accept", "init_data"},
 			ExposeHeaders:    []string{"Content-Length"},
